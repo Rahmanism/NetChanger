a188cbd02e2d160c7b759aa1b9196d95cbfcc98e
smrahmani@live.com <smrahmani@live.com>
2018-05-31 19:17:33 +0430

    Now we don't say we need administrator rights in manifest so we can publish and have an installer. Instead we check admin rights in Program main method.

--- NetChanger/Program.cs
+++ NetChanger/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Security.Principal;
 using System.Windows.Forms;
 
 namespace NetChanger
@@ -13,12 +16,43 @@ namespace NetChanger
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault( false );
+            var wi = WindowsIdentity.GetCurrent();
+            var wp = new WindowsPrincipal( wi );
 
-            operations = new Operations();
+            bool runAsAdmin = wp.IsInRole( WindowsBuiltInRole.Administrator );
 
-            Application.Run();
+            if ( !runAsAdmin ) {
+                // It is not possible to launch a ClickOnce app as administrator directly,
+                // so instead we launch the app as administrator in a new process.
+                var processInfo = new ProcessStartInfo( Assembly.GetExecutingAssembly().CodeBase ) {
+
+                    // The following properties run the new process as administrator
+                    UseShellExecute = true,
+                    Verb = "runas"
+                };
+
+                // Start the new process
+                try {
+                    Process.Start( processInfo );
+                }
+                catch ( Exception ) {
+                    // The user did not allow the application to run as administrator
+                    MessageBox.Show( "Sorry, but I don't seem to be able to start " +
+                       "this program with administrator rights!" );
+                }
+
+                // Shut down the current process
+                Application.Exit();
+            }
+            else {
+                // We are running as administrator
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault( false );
+
+                operations = new Operations();
+
+                Application.Run();
+            }
         }
     }
 }
